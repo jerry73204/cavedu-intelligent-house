b936ed8948ee1dec2bab53b29a924d27d3fde3ad
jerry73204 <jerry73204@gmail.com>
2016-03-02 03:22:17 +0800

    Create barebone event listening system in part3

--- part3/main.py
+++ part3/main.py
@@ -1,23 +1,88 @@
 #!/usr/bin/env python2
-import RPi.GPIO as GPIO
+# -*- coding: utf-8 -*-
 
+import time
+import logging
+import signal
+
+# flag indicating if any signal is received
+SHUTDOWN_FLAG = False
+
+# state constants
 STATE_OPEN      = 0
 STATE_CLOSED    = 1
 STATE_INVADED   = 2
 STATE_EMERGENCY = 3
 
-if __name__ == '__main__':
-    state = STATE_CLOSED
+# utility functions
+def is_door_open():
+    # TODO implementation
+    return True
 
+def is_authenticated():
+    # TODO implementation
+    return True
+
+def is_signaled_emergency():
+    # TODO implementation
+    return True
+
+# event handlers
+def on_auth():
+    # TODO implementation
+    pass
+
+def on_housebreaking():
+    # TODO implementation
+    pass
+
+def on_emergency():
+    # TODO implementation
+    pass
+
+def on_door_close():
+    # TODO implementation
+    pass
+
+def main():
+    prev_door_state = is_door_open()
+    state = STATE_OPEN if prev_door_state else STATE_CLOSED
+
+    # monitor the events by polling
     while True:
-        if state == STATE_OPEN:
-            pass
+        if SHUTDOWN_FLAG:
+            logging.info('Shutting down...')
+            exit()
 
-        elif state == STATE_CLOSED:
-            pass
+        elif is_signaled_emergency():
+            on_emergency(state)
 
-        elif state == STATE_INVADED:
-            pass
+        elif is_authenticated():
+            on_auth(state)
 
-        elif state == STATE_EMERGENCY:
-            pass
+        else:
+            curr_door_state = is_door_open()
+
+            if curr_door_state != prev_door_state:
+                if curr_door_state:
+                    on_housebreaking(state)
+                else:
+                    on_door_close(state)
+
+            prev_door_state = curr_door_state
+
+        time.sleep(50)
+
+def signal_handler(signum, frame):
+    global SHUTDOWN_FLAG
+    SHUTDOWN_FLAG = True
+
+if __name__ == '__main__':
+    # setup
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # run the main procedure
+    logging.info('Access control system started')
+    main()
