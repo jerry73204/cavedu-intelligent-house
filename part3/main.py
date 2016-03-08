e0252abf1d0817f3980d24a78d15b70268c5c96a
jerry73204 <jerry73204@gmail.com>
2016-03-08 12:11:52 +0800

    Finish some event handles

--- part3/main.py
+++ part3/main.py
@@ -8,6 +8,9 @@ import signal
 # flag indicating if any signal is received
 SHUTDOWN_FLAG = False
 
+# the delay time (ms) after each loop
+LOOP_DELAY = 50
+
 # state constants
 STATE_OPEN      = 0
 STATE_CLOSED    = 1
@@ -27,26 +30,45 @@ def is_signaled_emergency():
     # TODO implementation
     return True
 
-# event handlers
-def on_auth():
+def open_door():
     # TODO implementation
     pass
 
-def on_housebreaking():
+def warn_invaded():
     # TODO implementation
     pass
 
-def on_emergency():
-    # TODO implementation
-    pass
-
-def on_door_close():
+def warn_emergency():
     # TODO implementation
     pass
 
 def main():
     prev_door_state = is_door_open()
     state = STATE_OPEN if prev_door_state else STATE_CLOSED
+
+    # event handlers
+    def on_auth():
+        if state == STATE_OPEN:     # ignore this case
+            return
+
+        elif state == STATE_CLOSED: # open the door
+            open_door()
+            state = STATE_OPEN
+
+        elif state in (STATE_INVADED, STATE_EMERGENCY): # reset to closed state
+            state = STATE_CLOSED
+
+    def on_housebreaking():
+        state = STATE_INVADED
+        warn_invaded()
+
+    def on_emergency():
+        state = STATE_EMERGENCY
+        warn_emergency()
+
+    def on_door_close():
+        assert state == STATE_OPEN
+        state = STATE_CLOSED
 
     # monitor the events by polling
     while True:
@@ -55,23 +77,23 @@ def main():
             exit()
 
         elif is_signaled_emergency():
-            on_emergency(state)
+            on_emergency()
 
         elif is_authenticated():
-            on_auth(state)
+            on_auth()
 
         else:
             curr_door_state = is_door_open()
 
             if curr_door_state != prev_door_state:
                 if curr_door_state:
-                    on_housebreaking(state)
+                    on_housebreaking()
                 else:
-                    on_door_close(state)
+                    on_door_close()
 
             prev_door_state = curr_door_state
 
-        time.sleep(50)
+        time.sleep(LOOP_DELAY)
 
 def signal_handler(signum, frame):
     global SHUTDOWN_FLAG
