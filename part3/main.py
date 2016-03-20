6c6c27d2850a79f4e7bedc64aea22b35f7518991
jerry73204 <jerry73204@gmail.com>
2016-03-20 10:08:17 +0800

    Prevent from receiving false continuous signals

--- part3/main.py
+++ part3/main.py
@@ -32,6 +32,9 @@ serial_device = serial.Serial(port=config.SERIAL_DEVICE_PATH,
 prev_door_state = False
 state = STATE_CLOSED
 
+last_time_signaled_train_face = 0
+last_time_signaled_recognize_face = 0
+
 # utility functions
 def is_door_open():
     # TODO implementation
@@ -41,7 +44,7 @@ def is_authenticated():
     if not auth.FLAG_BUSY:
         if auth.FLAG_RECOGNITION_RESULT:
             auth.FLAG_RECOGNITION_RESULT = False
-            return auth.LAST_RECOGNITION_TIME + 5 <= time.time()
+            return time.time() <= auth.LAST_RECOGNITION_TIME + 5
         else:
             return False
 
@@ -51,12 +54,28 @@ def is_signaled_emergency():
 
 def is_signaled_train_face():
     # TODO implementation
-    return time.time() % 60 <= 1 # dummy impl.
+    global last_time_signaled_train_face
+    if time.time() < last_time_signaled_train_face + 1:
+        return False
+
+    result = time.time() % 25 <= 0.3 # dummy impl.
+    if result:
+        last_time_signaled_train_face = time.time()
+
+    return result
 
 def is_signaled_recognize_face():
     # TODO implementation
-    x = time.time() % 60
-    return x >= 50 and x <= 51  # dummy impl.
+    global last_time_signaled_recognize_face
+    if time.time() < last_time_signaled_recognize_face + 1:
+        return False
+
+    x = time.time() % 25
+    result = x >= 20 and x <= 20.3  # dummy impl.
+    if result:
+        last_time_signaled_recognize_face = time.time()
+
+    return result
 
 def open_door():
     # TODO implementation
