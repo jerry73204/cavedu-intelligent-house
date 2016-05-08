038b2f3d4f14c38c5f547b378d67b5407774de9d
wei851010 <wei851010@cavedu.com>
2016-05-08 21:34:39 +0800

    Added files via upload

---
+++ part1/main/handdle.ino
@@ -0,0 +1,18 @@
+int pin_gas_handdle = 4;
+int pin_door_open = 13;
+
+void handdle_setup(){
+  pinMode(pin_gas_handdle,OUTPUT);
+  pinMode(pin_door_open,OUTPUT);
+}
+
+void gas(boolean power){
+  digitalWrite(pin_gas_handdle,power);
+}
+
+void door(boolean power){
+  digitalWrite(pin_door_open,power);
+}
+
+
+
