4bb86ff9f333aa7f1de404ca024b4655336ca6a4
jerry73204 <jerry73204@gmail.com>
2016-05-10 00:59:48 +0800

    Add event triggering functionality

--- house-worker/house_worker.py
+++ house-worker/house_worker.py
@@ -5,13 +5,17 @@ import logging
 import signal
 
 # import RPi.GPIO as GPIO
+import serial
 import tornado.web
 import tornado.ioloop
+import tornado.httpclient
 
 import config
 import constants
 import __version__
 
+MASTER_ADDRESS = None
+MASTER_PORT = None
 GPIO_INPUT_PINS_WITH_SAVED_VALUES = dict()
 GPIO_OUTPUT_PINS = set()
 SERIAL_DEVICES = dict()
@@ -34,9 +38,11 @@ class UpdateConfigHandler(tornado.web.RequestHandler):
 
         global GPIO_INPUT_PINS_WITH_SAVED_VALUES
         global GPIO_OUTPUT_PINS
+        global SERIAL_DEVICES
 
         data = tornado.escape.json_decode(self.request.body)
 
+        # verify GPIO configuration
         gpio_input_pins = set(data['gpio']['inputs'])
         gpio_output_pins = set(data['gpio']['outputs'])
 
@@ -54,10 +60,12 @@ class UpdateConfigHandler(tornado.web.RequestHandler):
             if pin not in constants.GPIO_PIN_NUMBERS:
                 error_response('%d is not a valid GPIO pin number' % pin)
                 return
-            # GPIO.setup(pin, GPIO.OUT)
 
         # update GPIO output pins
         GPIO_OUTPUT_PINS = gpio_output_pins
+        for pin in gpio_output_pins:
+            # GPIO.setup(pin, GPIO.OUT)
+            pass
 
         # update GPIO input pins
         for removed_pins in GPIO_INPUT_PINS_WITH_SAVED_VALUES.keys - gpio_input_pins:
@@ -65,6 +73,31 @@ class UpdateConfigHandler(tornado.web.RequestHandler):
 
         for inserted_pins in gpio_input_pins - GPIO_INPUT_PINS_WITH_SAVED_VALUES.keys():
             GPIO_INPUT_PINS_WITH_SAVED_VALUES[inserted_pins] = None
+            # GPIO.setup(pin, GPIO.IN)
+
+        # update serial device configuration
+        for device_path, serial_object in SERIAL_DEVICES:
+            serial_object.close()
+        SERIAL_DEVICES = dict()
+
+        serial_devices = dict()
+        for device_path, baudrate in data['serial']:
+            if device_path in serial_devices:
+                error_response('the serial device "%s" is specified more than once' % device_path)
+                return
+
+            try:
+                serial_object = serial.Serial(port=device_path, baudrate=baudrate)
+                serial_devices[device_path] = serial_object
+
+            except serial.SerialException:
+                for serial_obj in serial_devices.values():
+                    serial_obj.close()
+
+                error_response('failed to open serial port "%s"' % device_path)
+                return
+
+        SERIAL_DEVICES = serial_devices
 
 class GpioReadHandler(tornado.web.RequestHandler):
     def get(self, pin_number):
@@ -105,7 +138,7 @@ class GpioWriteHandler(tornado.web.RequestHandler):
         self.write(response)
 
 class SerialReadHandler(tornado.web.RequestHandler):
-    def get(self, device_path, read_size):
+    def get(self):
         def error_response(reason):
             response = {
                 'status': 'error',
@@ -113,7 +146,10 @@ class SerialReadHandler(tornado.web.RequestHandler):
             }
             self.write(response)
 
-        read_size = int(read_size)
+        request_data = tornado.escape.json_decode(self.request.body)
+        read_size = request_data['size']
+        device_path = request_data['path']
+
         if read_size < 0:
             error_response('%d is not a valid buffer size' % read_size)
             return
@@ -132,7 +168,7 @@ class SerialReadHandler(tornado.web.RequestHandler):
         self.write(response)
 
 class SerialWriteHandler(tornado.web.RequestHandler):
-    def get(self, device_path):
+    def get(self):
         def error_response(reason):
             response = {
                 'status': 'error',
@@ -140,36 +176,58 @@ class SerialWriteHandler(tornado.web.RequestHandler):
             }
             self.write(response)
 
+        request_data = tornado.escape.json_decode(self.request.body)
+        device_path = request_data['path']
+        payload = request_data['data']
+
         if device_path not in SERIAL_DEVICES:
             error_response('"%s" is not a valid serial device')
             return
 
-        data = self.request.body
-
         serial_object = SERIAL_DEVICES[device_path]
-        serial_object.write(data)
+        serial_object.write(payload)
 
         response = {
             'status': 'ok',
         }
         self.write(response)
 
-def house_periodic_worker():
-    global GPIO_INPUT_PINS_WITH_SAVED_VALUES
+def trigger_event_on_master(event, data):
+    url = 'http://%s:%d/api' % (MASTER_ADDRESS, MASTER_PORT)
+    body = {
+        'event': event,
+        'data': data
+    }
 
+    request = tornado.httpclient.HTTPRequest(url, body=body)
+
+    try:
+        client = tornado.httpclient.AsyncHTTPClient()
+        client.fetch(request)
+
+    except tornado.httpclient.HTTPError:
+        logging.error('cannot connect to address "http://%s:%d/api"',MASTER_ADDRESS, MASTER_PORT)
+
+def house_periodic_worker():
     for pin, saved_value in enumerate(GPIO_INPUT_PINS_WITH_SAVED_VALUES):
         curr_value = 0
         # curr_value = GPIO.input(pin)
         GPIO_INPUT_PINS_WITH_SAVED_VALUES[pin] = curr_value
 
         if saved_value is not None and saved_value != curr_value:
-            # TODO trigger change event
-            pass
+            data = {
+                'pin': pin,
+                'from': saved_value,
+                'to': curr_value
+            }
+            trigger_event_on_master('gpio_input_change', data)
 
     for device_path, serial_object in SERIAL_DEVICES:
         if serial_object.in_waitint() > 0:
-            # TODO trigger serial eventx
-            pass
+            data = {
+                'path': device_path
+            }
+            trigger_event_on_master('serial_has_input', data)
 
 def signal_handler(signum, frame):
     # constants.SHUTDOWN_FLAG = True
@@ -182,10 +240,12 @@ def main():
     tornado.log.enable_pretty_logging()
 
     app = tornado.web.Application([
-        (r"/info", InfoHandler),
-        (r"/update-config", UpdateConfigHandler),
-        (r"/gpio/read/(\d+)", GpioReadHandler),
-        (r"/gpio/write/(\d+)/(0|1)", GpioWriteHandler),
+        (r'/info', InfoHandler),
+        (r'/update-config', UpdateConfigHandler),
+        (r'/gpio/read/(\d+)', GpioReadHandler),
+        (r'/gpio/write/(\d+)/(0|1)', GpioWriteHandler),
+        (r'/serial/read', SerialReadHandler),
+        (r'/serial/write', SerialWriteHandler),
     ])
     app.listen(config.LISTEN_PORT)
 
