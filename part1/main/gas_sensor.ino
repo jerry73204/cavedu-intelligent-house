93bc04c5346f0e449e7a84bcfb0052a940c46053
jerry73204 <jerry73204@gmail.com>
2016-05-14 21:06:55 +0800

    Code clean up

--- part1/main/gas_sensor.ino
+++ part1/main/gas_sensor.ino
@@ -3,6 +3,6 @@ int pin_gas_sensor = A0;
 int gas_sensor(){
   float sensor_volt = analogRead(pin_gas_sensor)*5.0/1024;
   float RS_gas = (5.0-sensor_volt)/sensor_volt;
-  float ratio = RS_gas/3.0; 
+  float ratio = RS_gas/3.0;
   return ratio;
 }
