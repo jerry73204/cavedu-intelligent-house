b70ce0d2b72c570fa9b0f74b63001799dae51886
wei851010 <wei851010@cavedu.com>
2016-05-15 11:34:51 -0500

    Add files via upload

--- part1/main/mcs.ino
+++ part1/main/mcs.ino
@@ -1,9 +1,9 @@
-void mcs(char s,int geo){
+void mcs(char s,int geo,float gas_value){
   Serial1.print(s);
   Serial1.print(",");
-  Serial1.print(gas_sensor());
+  Serial1.print(gas_value);
   Serial1.print(",");
-  Serial1.print(flame_sensor1());
+  Serial1.print(flame_sensor());
   Serial1.print(",");
   Serial1.print(geo);
 }
--- part1/main/warning.ino
+++ part1/main/warning.ino
@@ -3,11 +3,63 @@ int pin_buzz = 3;
 void warning_setup(){
   pinMode(pin_buzz,OUTPUT);
 }
-void buzz(){
+void buzzE(){
+  for(int i = 0;i < 7;i++){
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(200);
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(200);
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(400);
+  }
+}
+void buzzF(){
   for(int i = 0;i < 10;i++){
       digitalWrite(pin_buzz,HIGH);
-      delay(500);
+      delay(200);
       digitalWrite(pin_buzz,LOW);
-      delay(500);
+      delay(200);
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(400);
+  }
+}
+void buzzG(){
+  for(int i = 0;i < 6;i++){
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(200);
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(400);
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(400);
+  }
+}
+void buzzI(){
+  for(int i = 0;i < 33;i++){
+      digitalWrite(pin_buzz,HIGH);
+      delay(200);
+      digitalWrite(pin_buzz,LOW);
+      delay(100);
+  }
+}
+void buzzD(){
+  for(int i = 0;i < 16;i++){
+      digitalWrite(pin_buzz,HIGH);
+      delay(400);
+      digitalWrite(pin_buzz,LOW);
+      delay(200);
   }
 }
